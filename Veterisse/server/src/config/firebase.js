61aef3046d6ca999e21149a9ab857430111fe4ae
lBlackDev <rolando3365@gmail.com>
2025-05-04 22:37:34 -0400

    v1.2

---
+++ Veterisse/server/src/config/firebase.js
@@ -0,0 +1,13 @@
+// server/src/config/firebase.js
+
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+// Carga la configuración
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+module.exports = { db };
--- Veterisse/server/src/index.js
+++ Veterisse/server/src/index.js
@@ -1,23 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const cors    = require('cors');
-const pool    = require('./config/VeterisseBD');
+const { db } = require('./config/firebase');
 
 const app = express();
 app.use(cors());
+const PORT = process.env.PORT || 4000;
+
 app.use(express.json());
 // Test de conexión raiz
 app.get('/', async (_, res) => {
     try {
-      const [rows] = await pool.query('select * from veterissedb.item');
-      res.json(rows);
+      const snapshot = await db.collection('item').get();
+      const items = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      res.json(items);
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: 'Fallo al consultar la BD', details: err.message });
     }
   });
 
-const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`🚀 API escuchando en http://localhost:${PORT}/`);
 });
